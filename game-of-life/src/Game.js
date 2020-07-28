29d71364ace1c931f93486a429d1c4dcc27a6cc3
Virginia Wilcox <wilcoxva@gmail.com>
2020-07-27 20:57:38 -0400

    added grid

--- game-of-life/src/Game.js
+++ game-of-life/src/Game.js
@@ -9,49 +9,24 @@ import './Game.css';
 // Using an object to store the state will allow you to add more properties later that you didn't consider at first, reducing rewrite
 // Once you have grid set up you can bind UI to it
 
-const ht = 600;
-const wth = 800;
 const cell = 20;
+const width = 800;
+const height = 600;
 
 class Game extends React.Component {
     constructor() {
         super();
-        this.row = ht / cell;
-        this.col = wth / cell;
-
-        this.grid = this.makeGrid();
     }
 
     state = {
-        cells: [],
     }
-
-    makeGrid() {
-        let grid = [];
-        for (let i=0; i < this.row; i++) {
-            grid[i] = [];
-            for (let j = 0; j < this.col; j++) {
-                grid[i][j] = false;
-            };
-        };
-        return grid;
-    };
-
-    makeCells() {
-        let cells = [];
-        for (let i = 0; i < this.row; i++) {
-            for (let j = 0; j < this.col; j++) {
-                if (this.grid[i][j]) {
-                    cells.push({ j,i });
-                };
-            };
-        };
-        return cells;
-    };
 
     render() {
         return (
             <div>
+                <div className="Board"
+                    style={{ width: width, height: height, backgroundSize: `${cell}px ${cell}px` }}>
+                </div>
             </div>
         )
     }
